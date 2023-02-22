f3b3df6d930d4de0349f7d7a67e6afe7b1808d0d
Markus Horstmann <markushorstmann@hotmail.com>
2023-02-21 17:58:35 -0800

    Hello World: show how to load multiple nodesets, with and without resolver

--- src/Tests/CESMII.NodeSetUtilities.Tests/HelloWorld.cs
+++ src/Tests/CESMII.NodeSetUtilities.Tests/HelloWorld.cs
@@ -1,27 +1,15 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.Xml.Serialization;
-using System.Xml;
+using CESMII.OpcUa.NodeSetImporter;
 using CESMII.OpcUa.NodeSetModel;
 using CESMII.OpcUa.NodeSetModel.Factory.Opc;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging.Abstractions;
-using NodeSetDiff;
 using Opc.Ua;
 using Opc.Ua.Export;
-using Org.XmlUnit.Diff;
 using Xunit;
 using Xunit.Abstractions;
-using Xunit.Sdk;
-using static System.Net.WebRequestMethods;
-using File = System.IO.File;
 
 namespace CESMII.NodeSetUtilities.Tests
 {
@@ -39,17 +27,19 @@ namespace CESMII.NodeSetUtilities.Tests
         [Fact]
         public async Task HelloWorld()
         {
-            var file = Path.Combine(strTestNodeSetDirectory, "opcfoundation.org.UA.NodeSet2.xml");
-            output.WriteLine($"Importing {file}");
-
-            var nodesetXml = File.ReadAllText(file);
-
+            // Set up the importer
             var importer = new UANodeSetModelImporter(NullLogger.Instance);
 
+            // Read and import the base nodeset
+            var file = Path.Combine(strTestNodeSetDirectory, "opcfoundation.org.UA.NodeSet2.xml");
+            var nodesetXml = File.ReadAllText(file);
             var baseNodeSets = (await importer.ImportNodeSetModelAsync(nodesetXml)).ToDictionary(n => n.ModelUri);
+
+            // All required models are loaded: now we can use them to build a new model
+
             var uaBaseModel = baseNodeSets[Namespaces.OpcUa];
 
-            var nodeSetModel = new NodeSetModel
+            var myNodeSetModel = new NodeSetModel
             {
                 ModelUri = "https://opcua.rocks/UA",
             };
@@ -60,14 +50,14 @@ namespace CESMII.NodeSetUtilities.Tests
             {
                 DisplayName = new List<NodeModel.LocalizedText> { "AnimalType" },
                 SuperType = uaBaseModel.ObjectTypes.FirstOrDefault(ot => ot.BrowseName.EndsWith("BaseObjectType")),
-                NodeSet = nodeSetModel,
-                NodeId = new ExpandedNodeId(nextNodeId++, nodeSetModel.ModelUri).ToString(),
+                NodeSet = myNodeSetModel,
+                NodeId = new ExpandedNodeId(nextNodeId++, myNodeSetModel.ModelUri).ToString(),
                 Properties = new List<VariableModel>
                 {
                     new PropertyModel
                     {
-                        NodeSet = nodeSetModel,
-                        NodeId = new ExpandedNodeId(nextNodeId++, nodeSetModel.ModelUri).ToString(),
+                        NodeSet = myNodeSetModel,
+                        NodeId = new ExpandedNodeId(nextNodeId++, myNodeSetModel.ModelUri).ToString(),
                         DisplayName = new List<NodeModel.LocalizedText> { "Name" },
                         DataType = uaBaseModel.DataTypes.FirstOrDefault(ot => ot.BrowseName.EndsWith("String")),
                     },
@@ -76,8 +66,8 @@ namespace CESMII.NodeSetUtilities.Tests
                 {
                     new DataVariableModel
                     {
-                        NodeSet = nodeSetModel,
-                        NodeId = new ExpandedNodeId(nextNodeId++, nodeSetModel.ModelUri).ToString(),
+                        NodeSet = myNodeSetModel,
+                        NodeId = new ExpandedNodeId(nextNodeId++, myNodeSetModel.ModelUri).ToString(),
                         DisplayName = new List<NodeModel.LocalizedText> { "Height" },
                         DataType = uaBaseModel.DataTypes.FirstOrDefault(ot => ot.BrowseName.EndsWith("Float")),
                         EngineeringUnit = new VariableModel.EngineeringUnitInfo { DisplayName = "metre"  },
@@ -86,11 +76,153 @@ namespace CESMII.NodeSetUtilities.Tests
                 },
             };
 
-            nodeSetModel.ObjectTypes.Add(animalType);
+            myNodeSetModel.ObjectTypes.Add(animalType);
 
-            nodeSetModel.UpdateIndices();
+            myNodeSetModel.UpdateIndices();
+            var exportedNodeSetXml = UANodeSetModelExporter.ExportNodeSetAsXml(myNodeSetModel, baseNodeSets);
+        }
 
-            var exportedNodeSetXml = UANodeSetModelExporter.ExportNodeSetAsXml(nodeSetModel, baseNodeSets);
+        [Fact]
+        public async Task HelloManyWorld()
+        {
+            // Set up the importer
+            var importer = new UANodeSetModelImporter(NullLogger.Instance);
+            Dictionary<string, NodeSetModel> nodeSetModels = new();
+            var opcContext = new DefaultOpcUaContext(nodeSetModels, NullLogger.Instance);
+
+            // Read and import the base nodeset
+            var file = Path.Combine(strTestNodeSetDirectory, "opcfoundation.org.UA.NodeSet2.xml");
+            var nodeSet = UANodeSet.Read(new FileStream(file, FileMode.Open));
+            await importer.LoadNodeSetModelAsync(opcContext, nodeSet);
+
+            // Read and import DI nodeset
+            file = Path.Combine(strTestNodeSetDirectory, "opcfoundation.org.UA.DI.NodeSet2.xml");
+            nodeSet = UANodeSet.Read(new FileStream(file, FileMode.Open));
+            await importer.LoadNodeSetModelAsync(opcContext, nodeSet);
+
+            // Read and import Robotics nodeset
+            file = Path.Combine(strTestNodeSetDirectory, "opcfoundation.org.UA.Robotics.NodeSet2.xml");
+            nodeSet = UANodeSet.Read(new FileStream(file, FileMode.Open));
+            await importer.LoadNodeSetModelAsync(opcContext, nodeSet);
+
+            // Read and import HumanRobot nodeset
+            file = Path.Combine(strTestNodeSetDirectory, "clabs.com.UA.HumanRobot.NodeSet2.xml");
+            nodeSet = UANodeSet.Read(new FileStream(file, FileMode.Open));
+            await importer.LoadNodeSetModelAsync(opcContext, nodeSet);
+
+            // All required models are loaded: now we can use them to build a new model
+            var uaBaseModel = nodeSetModels[Namespaces.OpcUa];
+            var uaHumanRobotModel = nodeSetModels["http://clabs.com/UA/HumanRobot"];
+
+            var myNodeSetModel = new NodeSetModel
+            {
+                ModelUri = "https://opcua.rocks/UA",
+            };
+
+            uint nextNodeId = 1000;
+
+            var animalType = new ObjectTypeModel
+            {
+                DisplayName = new List<NodeModel.LocalizedText> { "AnimalType" },
+                SuperType = uaHumanRobotModel.ObjectTypes.FirstOrDefault(ot => ot.BrowseName.EndsWith("RobotEye")),
+                NodeSet = myNodeSetModel,
+                NodeId = new ExpandedNodeId(nextNodeId++, myNodeSetModel.ModelUri).ToString(),
+                Properties = new List<VariableModel>
+                {
+                    new PropertyModel
+                    {
+                        NodeSet = myNodeSetModel,
+                        NodeId = new ExpandedNodeId(nextNodeId++, myNodeSetModel.ModelUri).ToString(),
+                        DisplayName = new List<NodeModel.LocalizedText> { "Name" },
+                        DataType = uaBaseModel.DataTypes.FirstOrDefault(ot => ot.BrowseName.EndsWith("String")),
+                    },
+                },
+                DataVariables = new List<DataVariableModel>
+                {
+                    new DataVariableModel
+                    {
+                        NodeSet = myNodeSetModel,
+                        NodeId = new ExpandedNodeId(nextNodeId++, myNodeSetModel.ModelUri).ToString(),
+                        DisplayName = new List<NodeModel.LocalizedText> { "Height" },
+                        DataType = uaBaseModel.DataTypes.FirstOrDefault(ot => ot.BrowseName.EndsWith("Float")),
+                        EngineeringUnit = new VariableModel.EngineeringUnitInfo { DisplayName = "metre"  },
+                        Value = JsonEncodeVariant((float) 0),
+                    },
+                },
+            };
+
+            myNodeSetModel.ObjectTypes.Add(animalType);
+
+            // the new nodeset model is ready: export it to XML
+            myNodeSetModel.UpdateIndices();
+            var exportedNodeSetXml = UANodeSetModelExporter.ExportNodeSetAsXml(myNodeSetModel, nodeSetModels);
+        }
+
+
+        [Fact]
+        public async Task HelloManyWorldResolver()
+        {
+            // The resolver's ResolveNodeSetsAsync is called to load any required nodeset XML
+            // The resolver in this sample tries to find missing nodesets in the TestNodeSets directory
+            // The Profile Designer project contains a resolver that reads from an OPC UA Cloud Library (https://github.com/cesmii/ProfileDesigner/tree/main/api/CESMII.OpcUa.CloudLibraryResolver)
+            var resolver = new MyDependencyResolver();
+
+            // The importer handles resolving and loading of nodeset XMLs
+            var importer = new UANodeSetModelImporter(NullLogger.Instance, resolver);
+
+            // Read the nodest file
+            var file = Path.Combine(strTestNodeSetDirectory, "clabs.com.UA.HumanRobot.NodeSet2.xml");
+            var nodesetXml = File.ReadAllText(file);
+
+            // Load the Human Robot nodeset as a model, resolving any required dependencies using MyDependencyResolver
+            var nodeSetModels = (await importer.ImportNodeSetModelAsync(nodesetXml, loadAllDependentModels: true)).ToDictionary(n => n.ModelUri);
+
+            // All required models are loaded: now we can use them to build a new model
+            var uaBaseModel = nodeSetModels[Namespaces.OpcUa];
+            var uaHumanRobotModel = nodeSetModels["http://clabs.com/UA/HumanRobot"];
+
+            var myNodeSetModel = new NodeSetModel
+            {
+                ModelUri = "https://opcua.rocks/UA",
+            };
+
+            uint nextNodeId = 1000;
+
+            var animalType = new ObjectTypeModel
+            {
+                DisplayName = new List<NodeModel.LocalizedText> { "AnimalType" },
+                SuperType = uaHumanRobotModel.ObjectTypes.FirstOrDefault(ot => ot.BrowseName.EndsWith("RobotEye")),
+                NodeSet = myNodeSetModel,
+                NodeId = new ExpandedNodeId(nextNodeId++, myNodeSetModel.ModelUri).ToString(),
+                Properties = new List<VariableModel>
+                {
+                    new PropertyModel
+                    {
+                        NodeSet = myNodeSetModel,
+                        NodeId = new ExpandedNodeId(nextNodeId++, myNodeSetModel.ModelUri).ToString(),
+                        DisplayName = new List<NodeModel.LocalizedText> { "Name" },
+                        DataType = uaBaseModel.DataTypes.FirstOrDefault(ot => ot.BrowseName.EndsWith("String")),
+                    },
+                },
+                DataVariables = new List<DataVariableModel>
+                {
+                    new DataVariableModel
+                    {
+                        NodeSet = myNodeSetModel,
+                        NodeId = new ExpandedNodeId(nextNodeId++, myNodeSetModel.ModelUri).ToString(),
+                        DisplayName = new List<NodeModel.LocalizedText> { "Height" },
+                        DataType = uaBaseModel.DataTypes.FirstOrDefault(ot => ot.BrowseName.EndsWith("Float")),
+                        EngineeringUnit = new VariableModel.EngineeringUnitInfo { DisplayName = "metre"  },
+                        Value = JsonEncodeVariant((float) 0),
+                    },
+                },
+            };
+
+            myNodeSetModel.ObjectTypes.Add(animalType);
+
+            // the new nodeset model is ready: export it to XML
+            myNodeSetModel.UpdateIndices();
+            var exportedNodeSetXml = UANodeSetModelExporter.ExportNodeSetAsXml(myNodeSetModel, nodeSetModels);
         }
 
         private string JsonEncodeVariant(Variant v)
@@ -101,6 +233,29 @@ namespace CESMII.NodeSetUtilities.Tests
                 var jsonValue = encoder.CloseAndReturnText();
                 return jsonValue;
             }
+        }
+    }
+
+    internal class MyDependencyResolver : IUANodeSetResolver
+    {
+        public Task<IEnumerable<string>> ResolveNodeSetsAsync(List<ModelNameAndVersion> missingModels)
+        {
+            List<string> nodeSetsXml = new();
+            foreach (var missingModel in missingModels)
+            {
+                var fileName = missingModel.ModelUri.Replace("http://", "").Replace("/", ".");
+                if (!fileName.EndsWith("."))
+                {
+                    fileName += ".";
+                }
+                fileName += "NodeSet2.xml";
+
+                var filePath = Path.Combine(HelloWorldTest.strTestNodeSetDirectory, fileName);
+
+                var nodeSetXml = File.ReadAllText(filePath);
+                nodeSetsXml.Add(nodeSetXml);
+            }
+            return Task.FromResult<IEnumerable<string>>(nodeSetsXml);
         }
     }
 }
